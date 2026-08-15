5c3b921bc0b9215750312f61bb8fc054c3bffcb2
Haiitech <daniilmedvedev150711@gmail.com>
2026-08-16 00:19:25 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,19 @@
-const CACHE_NAME = 'dani-messenger-v1';
+const CACHE_NAME = 'dani-mesend-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html'
 ];
 
-// Установка Service Worker и кэширование базовых файлов
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Перехват запросов для работы в офлайн-режиме
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
